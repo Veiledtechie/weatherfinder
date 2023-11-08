34339be68d47110ccff38e595b075b4b24969ba3
Veiledtechie <Veiledtechie@users.noreply.github.com>
2023-11-08 12:59:31 +0100

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature-js");
   let temperature = response.data.temperature.current;
+
   temperatureElement.innerHTML = Math.round(temperature);
+
   let cityElement = document.querySelector("#city-js");
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "tc6b22c02231b7009184f4b92obd4a5f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -20,4 +51,4 @@ function handleSearchSubmit(event) {
 }
 let searchFormElement = document.querySelector("#search-form-js");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Nigeria");
+searchCity("Ibadan");
