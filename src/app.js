056ccab3c159c046412862a21f597a0f3bdfc096
Veiledtechie <Veiledtechie@users.noreply.github.com>
2023-11-17 12:59:27 +0100

    Make the forecast active in JS and styled in CSS

--- src/app.js
+++ src/app.js
@@ -57,6 +57,12 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 
 function getForecast(city) {
   let apiKey = "tc6b22c02231b7009184f4b92obd4a5f";
@@ -64,27 +70,31 @@ function getForecast(city) {
   axios(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
   let forecastHtml = "";
-  days.forEach(function (days) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
       
-        <div class="weather-forecast-date">${days}</div>
-        <div class="weather-forecast-icon">☁</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+         
+       <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
         <div class="weather-forecast-temp">
           <span class="weather-forecast-temp-max">
-            <strong>12°</strong>
-          <span>
-          <span class="weather-forecast-temp-min">10°</span>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
+          </span>
+          <span class="weather-forecast-temp-min">${Math.round(
+            day.temperature.minimum
+          )}°</span>
         </div>
       
     </div>
   `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
